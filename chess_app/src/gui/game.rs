236f0366758441fabe1fd58a9a4fe14a39dc59b4
escobar-west <victorv.suriel@gmail.com>
2023-01-16 12:05:41 -0500

    update graphics to draw white king

--- chess_app/src/gui/game.rs
+++ chess_app/src/gui/game.rs
@@ -1,40 +1,53 @@
+use super::Scene;
 use macroquad::{
-    color::{Color, DARKGRAY, LIGHTGRAY, WHITE},
-    math::vec2,
-    shapes::draw_rectangle,
+    color::{DARKGRAY, LIGHTGRAY, WHITE},
+    math::{Rect, Vec2},
     text::draw_text,
     texture::{draw_texture_ex, load_texture, DrawTextureParams, Texture2D},
     window::{clear_background, next_frame, screen_height, screen_width},
 };
 
-use super::Scene;
-
-const SQUARES: u8 = 8;
-const LIGHT_COLOR: Color = Color::new(234. / 255., 233. / 255., 212. / 255., 1.);
-const DARK_COLOR: Color = Color::new(84. / 255., 114. / 255., 150. / 255., 1.);
-
 pub async fn game_scene() -> Scene {
     let path = "assets/boards/board.png";
     let board_texture: Texture2D = load_texture(path).await.unwrap();
+
+    let path = "assets/pieces/wiki_chess.png";
+    let piece_texture: Texture2D = load_texture(path).await.unwrap();
     loop {
         clear_background(LIGHTGRAY);
-        let game_size = screen_width().min(screen_height());
-        let margin = (screen_height() - game_size) / 2. + 10.;
-        let sq_size = (screen_height() - margin) / SQUARES as f32;
+        let height = screen_height();
+        let width = screen_width();
+        let game_size = width.min(height);
 
         draw_texture_ex(
             board_texture,
-            (screen_width() - 8. * sq_size) / 2.,
-            margin / 2.,
+            (width - game_size) / 2.,
+            (height - game_size) / 2.,
             WHITE,
             DrawTextureParams {
-                dest_size: Some(vec2(8. * sq_size, 8. * sq_size)),
+                dest_size: Some(Vec2::splat(game_size)),
+                ..Default::default()
+            },
+        );
+        draw_texture_ex(
+            piece_texture,
+            (width - game_size) / 2.,
+            (height - game_size) / 2.,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::splat(game_size / 8.)),
+                source: Some(Rect {
+                    x: 0.,
+                    y: 0.,
+                    w: 170.,
+                    h: 170.,
+                }),
                 ..Default::default()
             },
         );
 
         draw_text("CLOCK", 20., 20., 20., DARKGRAY);
-        draw_text("CLOCK", 20., screen_height() - 20., 20., DARKGRAY);
+        draw_text("CLOCK", 20., height - 20., 20., DARKGRAY);
 
         next_frame().await
     }
