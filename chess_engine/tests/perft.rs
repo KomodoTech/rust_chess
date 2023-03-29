95241e30f8cda942888d2da913d552e037c75e04
alexandre leibler <alexandre.leibler@protonmail.com>
2023-03-29 01:15:17 -0400

    divided perft in place. Perft results are off so engine has bugs. First lead is exceeding pawn limit somehow

--- chess_engine/tests/perft.rs
+++ chess_engine/tests/perft.rs
@@ -1,4 +1,8 @@
-use chess_engine::gamestate::GamestateBuilder;
+use chess_engine::{
+    error::{GamestateValidityCheckError, MoveDeserializeError, MoveGenError, UndoMoveError},
+    gamestate::{Gamestate, GamestateBuilder, ValidityCheck},
+};
+use num::Zero;
 
 use std::{
     error::Error,
@@ -18,6 +22,18 @@ use thiserror::Error;
 pub enum PerftError {
     #[error(transparent)]
     PerftInitExpected(#[from] PerftInitExpectedError),
+
+    #[error(transparent)]
+    PerftGamestate(#[from] GamestateValidityCheckError),
+
+    #[error(transparent)]
+    PerftMoveGen(#[from] MoveGenError),
+
+    #[error(transparent)]
+    UndoMove(#[from] UndoMoveError),
+
+    #[error(transparent)]
+    MoveDeserialize(#[from] MoveDeserializeError),
 }
 
 #[derive(Error, Debug, PartialEq)]
@@ -47,6 +63,63 @@ impl Perft {
     }
 }
 
+fn perft(gamestate: &mut Gamestate, depth: usize, leaf_count: &mut u64) -> Result<u64, PerftError> {
+    gamestate.check_gamestate(ValidityCheck::Move)?;
+
+    // Base Case
+    if depth.is_zero() {
+        *leaf_count += 1;
+        return Ok(*leaf_count);
+    } else {
+        // Recursive Case
+        let move_list = gamestate.gen_move_list()?;
+        for move_ in move_list.moves.into_iter().flatten() {
+            if gamestate.make_move(move_).is_ok() {
+                perft(gamestate, depth - 1, leaf_count)?;
+                gamestate.undo_move()?;
+            }
+        }
+    }
+
+    Ok(*leaf_count)
+}
+
+fn divided_perft(
+    gamestate: &mut Gamestate,
+    depth: usize,
+    leaf_count: &mut u64,
+) -> Result<u64, PerftError> {
+    gamestate.check_gamestate(ValidityCheck::Move)?;
+
+    println!("{}", gamestate);
+    println!("PERFT TO DEPTH {}", depth);
+
+    *leaf_count = 0;
+
+    let move_list = gamestate.gen_move_list()?;
+    for (move_idx, move_) in move_list.moves.into_iter().flatten().enumerate() {
+        if gamestate.make_move(move_).is_ok() {
+            let mut total_count = *leaf_count;
+
+            perft(gamestate, depth - 1, leaf_count)?;
+            gamestate.undo_move()?;
+
+            let prev_delta_count = *leaf_count - total_count;
+            println!(
+                "Move {}: {}{} : {}",
+                move_idx,
+                move_.get_start()?,
+                move_.get_end()?,
+                prev_delta_count
+            );
+        }
+
+        println!("TOTAL NODES VISITED: {}", *leaf_count);
+    }
+
+    Ok(*leaf_count)
+}
+
 fn read_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -55,6 +128,8 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+/// Read in Perft results from pre-generated file. Create a Vec of Perft instances
+/// that we can use to compare to our move generation
 fn init_expected<P>(perft_expected_path: P) -> Result<Vec<Perft>, Box<dyn Error>>
 where
     P: AsRef<Path>,
@@ -92,9 +167,20 @@ where
 }
 
 #[test]
-fn perft() {
+fn test_perft() {
     let perft_expected_path = Path::new(PERFT_EXPECTED_PATH);
     let expected = init_expected(perft_expected_path).unwrap();
 
-    println!("{:#?}\n Testing {} FENs", expected, expected.len());
+    // println!("{:#?}\n Testing {} FENs", expected, expected.len());
+
+    let mut gamestate = GamestateBuilder::new_with_fen(
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+    )
+    .unwrap()
+    .build()
+    .unwrap();
+
+    let mut leaf_count: u64 = 0;
+    let node_count = divided_perft(&mut gamestate, 2, &mut leaf_count).unwrap();
+    println!("node_count: {}", node_count);
 }
